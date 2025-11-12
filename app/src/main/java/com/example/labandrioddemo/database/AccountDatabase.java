14204a4dd3321b1fa3d0f9538a4b15108bcbef51
Matthew Davis <matthewddavis18@gmail.com>
2025-11-12 11:08:09 -0800

    Started UserDAO Started database

--- app/src/main/java/com/example/labandrioddemo/database/AccountDatabase.java
+++ app/src/main/java/com/example/labandrioddemo/database/AccountDatabase.java
@@ -1,10 +1,64 @@
 package com.example.labandrioddemo.database;
 
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
+import android.content.Context;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.example.labandrioddemo.MainActivity;
 import com.example.labandrioddemo.database.entities.User;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Database(entities = {User.class}, version = 0, exportSchema = false)
 public abstract class AccountDatabase extends RoomDatabase {
+    public static final String USER_TABLE = "usertable";
+    private static final String DATABASE_NAME = "Accountdatabase";
+    private static volatile AccountDatabase INSTANCE;
+    private static final int NUMBER_OF_THREADS = 4;
+    static final ExecutorService databaseWriteExecutor = Executors.newFixedThreadPool(NUMBER_OF_THREADS);
+
+    static AccountDatabase getDatabase(final Context context) {
+        if(INSTANCE == null) {
+            synchronized (AccountDatabase.class) {
+                if(INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                                    context.getApplicationContext(),
+                                    AccountDatabase.class,
+                                    DATABASE_NAME
+                            )
+                            .fallbackToDestructiveMigration()
+                            .addCallback(addDefaultValues)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    private static final RoomDatabase.Callback addDefaultValues = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+//            Log.i(MainActivity.TAG, "Database Created!");
+            databaseWriteExecutor.execute(() -> {
+//                UserDAO dao = INSTANCE.userDAO();
+//                dao.deleteAll();
+
+//                User admin = new User("admin1", "admin1");
+//                admin.setAdmin(true);
+//                dao.insert(admin);
+//
+//                User testUser1 = new User("testuser1", "testuser1");
+//                dao.insert(testUser1);
+            });
+        }
+    };
+
+    public abstract UserDAO userDAO();
 }
