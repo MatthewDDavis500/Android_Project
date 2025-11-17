7e4ec802a7fed16e49d7d614b3436e05f4bf80b2
Eric Rubio Salazar <erubiosalazar@csumb.edu>
2025-11-17 11:26:25 -0800

    Minor changes to code as well as name change from village screen to battle screen

---
+++ app/src/main/java/com/example/labandrioddemo/battleScreeenActivity.java
@@ -0,0 +1,14 @@
+package com.example.labandrioddemo;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class battleScreeenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_battle_screen);
+    }
+}
