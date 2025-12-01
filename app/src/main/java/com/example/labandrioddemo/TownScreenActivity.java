1d5c58f1b797c03cc18bf201ef845998ffdd8fa3
Matthew Davis <matthewddavis18@gmail.com>
2025-12-01 14:44:56 -0800

    Refactored townScreen.java to TownScreenActivity.java

--- app/src/main/java/com/example/labandrioddemo/TownScreenActivity.java
+++ app/src/main/java/com/example/labandrioddemo/TownScreenActivity.java
@@ -2,13 +2,10 @@ package com.example.labandrioddemo;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class townScreen extends AppCompatActivity {
+public class TownScreenActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
