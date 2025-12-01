b805d60056d03db77f0ed5001280666a7327979b
Matthew Davis <matthewddavis18@gmail.com>
2025-12-01 15:32:46 -0800

    Refactored townShopActivity.java to TownShopActivity.java

--- app/src/main/java/com/example/labandrioddemo/TownShopActivity.java
+++ app/src/main/java/com/example/labandrioddemo/TownShopActivity.java
@@ -2,13 +2,9 @@ package com.example.labandrioddemo;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-public class townShopActivity extends AppCompatActivity {
+public class TownShopActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
