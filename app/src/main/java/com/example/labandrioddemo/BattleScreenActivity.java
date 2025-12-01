c78ccf118e510fd4e84d1adfd326b293b8bbec19
Hydra <licole@csumb.edu>
2025-12-01 15:12:54 -0800

    Created the attack button functionality and started on flee button.

--- app/src/main/java/com/example/labandrioddemo/BattleScreenActivity.java
+++ app/src/main/java/com/example/labandrioddemo/BattleScreenActivity.java
@@ -1,26 +1,27 @@
 package com.example.labandrioddemo;
 
 import static com.example.labandrioddemo.MainMenuActivity.COMP_DOOM_ACTIVITY_CHARACTER_ID;
-import static com.example.labandrioddemo.databinding.ActivityBattleScreenBinding.inflate;
-
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
-
 import com.example.labandrioddemo.database.AccountRepository;
 import com.example.labandrioddemo.database.entities.ProjectCharacter;
 import com.example.labandrioddemo.databinding.ActivityBattleScreenBinding;
+import java.util.Random;
 
 public class BattleScreenActivity extends AppCompatActivity {
 
     private ActivityBattleScreenBinding binding;
     private AccountRepository repository;
     private int battleNum;
+    private int monsterMaxHp;
+    private int monsterCurHp;
+
+    Random random = new Random();
 
     private ProjectCharacter character;
     private BattleScreenActivity thisHolder = this;
@@ -40,11 +41,45 @@ public class BattleScreenActivity extends AppCompatActivity {
                     thisHolder.character = character;
                     binding.hpTextView.setText("HP: " + character.getCurrHp() + "/" + character.getMaxHp());
                     binding.battleName.setText("Battle " + character.getBattleNum());
+                    monsterMaxHp = character.getBattleNum()*2 + 10;
+                    monsterCurHp = monsterMaxHp;
+                    binding.enemyHpTextView.setText("Enemy HP: " + monsterCurHp + "/" + monsterMaxHp);
+                    binding.currentSituationTextView.setText("A Monster Appears!");
                 }
             }
         });
         //Don't put stuff here, too slow ^
+
+        binding.attackButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (monsterCurHp > 0) {
+                    character.setCurrHp(character.getCurrHp() - random.nextInt(character.getBattleNum(),5 + character.getBattleNum()));
+                    binding.hpTextView.setText("HP: " + character.getCurrHp() + "/" + character.getMaxHp());
+                }
+                monsterCurHp = monsterCurHp - ( 5 + character.getAtkMod());
+                binding.enemyHpTextView.setText("Enemy HP: " + monsterCurHp + "/" + monsterMaxHp);
+                if (character.getCurrHp() > 0) {
+                    if (monsterCurHp > 0) {
+                        binding.currentSituationTextView.setText(character.getCharacterName() + " did and took damage.");
+                    }
+                    binding.currentSituationTextView.setText("You beat the monster! Flee to return to village");
+                } else {
+                    binding.currentSituationTextView.setText("You died! Flee to return to character creation");
+                }
+            }
+        });
+
+        binding.fleeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int chance = random.nextInt(0,10);
+                if (monsterCurHp <= 0)
+            }
+        });
     }
+
+
 
     static Intent BattleScreenIntentFactory(Context context, int characterId) {
         Intent intent = new Intent(context, BattleScreenActivity.class);
