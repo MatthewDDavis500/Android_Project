b2aa310e1b8e5e8473abfbfbbabaa1e1290e780c
Grenwyvern <steven.meck03@gmail.com>
2025-12-02 09:55:34 -0800

    First attempt of characterCreation. Immediately goes to characterSelectActivity when create chracter is accessed. Need to fix this

--- app/src/main/java/com/example/labandrioddemo/CharacterCreationActivity.java
+++ app/src/main/java/com/example/labandrioddemo/CharacterCreationActivity.java
@@ -3,17 +3,32 @@ package com.example.labandrioddemo;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.labandrioddemo.database.entities.ProjectCharacter;
+import com.example.labandrioddemo.databinding.ActivityCharacterCreationBinding;
+
 public class CharacterCreationActivity extends AppCompatActivity {
+    private ActivityCharacterCreationBinding binding;
+    private ProjectCharacter character;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_character_creation);
+        binding = ActivityCharacterCreationBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(CharacterSelectActivity.characterSelectIntentFactory(getApplicationContext(), character.getCharacterID()));
+            }
+        });
     }
 
     static Intent characterCreationIntentFactory(Context context) {
-        return new Intent(context, CharacterCreationActivity.class);
+        return new Intent(context, CharacterSelectActivity.class);
     }
 }
