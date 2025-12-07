aa27cc3d6aaff7b7fbaa3359552a04c1b75787d2
Matthew Davis <matthewddavis18@gmail.com>
2025-12-06 18:42:58 -0800

    Made unit test for successful health purchase

--- app/src/test/java/com/example/labandrioddemo/TownShopUnitTests.java
+++ app/src/test/java/com/example/labandrioddemo/TownShopUnitTests.java
@@ -37,10 +37,10 @@ public class TownShopUnitTests {
     }
 
     @Test
-    public void purchasingAttackSuccessful() {
+    public void purchaseAttackSuccessful() {
         // test initial values
         assertEquals(TEST_ATTACK_MODIFIER, testCharacter.getAtkMod());
-        assertEquals(TEST_GOLD, testCharacter.getGold());
+        assertTrue(TownShopActivity.ATTACK_COST <= testCharacter.getGold());
 
         // purchase attack upgrade
         townShop.buyAttack(testCharacter);
@@ -51,7 +51,7 @@ public class TownShopUnitTests {
     }
 
     @Test
-    public void purchasingAttackFailed() {
+    public void purchaseAttackFailed() {
         // set character gold to be not enough for the purchase
         testCharacter.setGold(TownShopActivity.ATTACK_COST - 1);
 
@@ -67,5 +67,19 @@ public class TownShopUnitTests {
         assertEquals(TownShopActivity.ATTACK_COST - 1, testCharacter.getGold());
     }
 
+    @Test
+    public void purchaseHealthSuccessful() {
+        // test initial values
+        assertEquals(TEST_CURRENT_HP, testCharacter.getCurrHp());
+        assertEquals(TEST_MAX_HP, testCharacter.getMaxHp());
+        assertTrue(TownShopActivity.HEALTH_COST <= testCharacter.getGold());
 
+        // purchase attack upgrade
+        townShop.buyHealth(testCharacter);
+
+        // test values after purchase
+        assertEquals(TEST_CURRENT_HP + 5, testCharacter.getCurrHp());
+        assertEquals(TEST_MAX_HP + 5, testCharacter.getMaxHp());
+        assertEquals(TEST_GOLD - TownShopActivity.HEALTH_COST, testCharacter.getGold());
+    }
 }
